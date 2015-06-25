8a71998637f2f2fe6243a404f6ce45e52ef7dbbf
nicolaslabbe <nicolas.labbe@adfab.fr>
2015-06-25 11:44:23 +0200

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,22 +2,22 @@
 # . ./.env
 
 error_exit() {
-  "$1" 1>&2 >> logs/deploy.txt
+  echo "$1" 1>&2 >> logs/deploy.txt
   exit 1
 }
 
 rm logs/deploy.txt
 pwd >> logs/deploy.txt
 
-pm2 restart webhook.js || error_exit "error restart webhook"
-sleep 2
-echo "\n pm2 restart webhook.js" >> logs/deploy.txt
-
 echo "\n" >> logs/deploy.txt
 git pull origin master >> logs/deploy.txt || error_exit "error git pull"
 sleep 2
 
 echo "deploying on ${NODE_ENV} with user ${USER} using $(node --version)" >> logs/deploy.txt
+
+pm2 restart webhook.js || error_exit "error restart webhook"
+sleep 2
+echo "\n pm2 restart webhook.js" >> logs/deploy.txt
 
 pm2 list || error_exit "error pm2 list"
 sleep 2
