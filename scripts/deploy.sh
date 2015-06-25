24c69cff97b797086a2d31e133edc9c49096a25d
nicolaslabbe <nicolas.labbe@adfab.fr>
2015-06-25 10:51:24 +0200

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,21 +5,29 @@ error_exit() {
   echo "$1" 1>&2
   exit 1
 }
+echo "\n\n\n\n\n" >> logs/deploy.txt
 
-echo "####################"
-echo "git pull            "
-echo "####################"
-git pull origin master
+# "####################"
+# "git pull            "
+# "####################"
+echo "\n" >> logs/deploy.txt
+git pull origin master >> logs/deploy.txt
 
 echo "deploying on ${NODE_ENV} with user ${USER} using $(node --version)"
 
-echo "####################"
-echo " start server       "
-echo "####################"
+# "####################"
+# " start server       "
+# "####################"
 
-pm2 stop webhook
+# "pm2 stop webhook"
+echo "\n" >> logs/deploy.txt
+pm2 stop webhook >> logs/deploy.txt
 sleep 1
-pm2 start webhook.js
+# "pm2 start webhook.js"
+echo "\n" >> logs/deploy.txt
+pm2 start webhook.js >> logs/deploy.txt
 
 sleep 2
-pm2 list
+# "pm2 list"
+echo "\n" >> logs/deploy.txt
+pm2 list >> logs/deploy.txt
