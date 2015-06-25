15dc5132767bebbe3be4f300f89fb0e9ddc5d1bb
nicolaslabbe <nicolas.labbe@adfab.fr>
2015-06-25 10:37:51 +0200

    add logs txt

--- webhook.js
+++ webhook.js
@@ -5,33 +5,55 @@ var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: 'pixelart' })
 var spawn = require('child_process').spawn
 var _ = require('underscore'); // for some utility goodness
+var fs = require('fs');
+var util = require('util');
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(7777)
+function writeLogs(log) {
+  var flags = 'a';
 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
-
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref)
-
-  var deploySh = spawn('sh', [ 'scripts/deploy.sh' ], {
-    cwd: '/home/nicolas/node-webhook',
-    env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
+  var stream = fs.createWriteStream("./logs/webhook.txt", { 'flags': flags, 'encoding': null, 'mode': 0666});
+  stream.once('open', function(fd) {
+    stream.write('[ ' + new Date() + ' ] ' + log);
+    stream.end();
   });
-})
+}
 
-handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title)
-})
+fs.readdir('logs', function(err, files) {
+  if(typeof files === 'undefined' || files === null)
+    fs.mkdir('logs');
+
+  writeLogs('------------------------------------------------------------');
+  writeLogs('start webhook ');
+
+  http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  }).listen(7777)
+
+  handler.on('error', function (err) {
+    util.debug('Error:', err.message)
+  })
+
+  handler.on('push', function (event) {
+    writeLogs('Received a push event for ' + event.payload.repository.name + ' to ' + event.payload.ref + '');
+    util.debug('Received a push event for %s to %s',
+      event.payload.repository.name,
+      event.payload.ref)
+
+    var deploySh = spawn('sh', [ 'scripts/deploy.sh' ], {
+      cwd: '/home/nicolas/node-webhook',
+      env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
+    });
+  })
+
+  handler.on('issues', function (event) {
+    writeLogs('Received an issue event for %s action=%s: #%d %s');
+    util.debug('Received an issue event for ' + event.payload.repository.name + ' action=' + event.payload.action + ': #' + event.payload.issue.number + ' ' + event.payload.issue.title,
+      event.payload.repository.name,
+      event.payload.action,
+      event.payload.issue.number,
+      event.payload.issue.title)
+  })
+});
