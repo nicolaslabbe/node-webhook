8252b607be1d294f7c37222c732da13290fe3e29
nicolaslabbe <nicolas.labbe@adfab.fr>
2015-06-25 11:35:07 +0200

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,14 +15,12 @@ sleep 2
 
 echo "deploying on ${NODE_ENV} with user ${USER} using $(node --version)" >> logs/deploy.txt
 
-echo "\n" >> logs/deploy.txt
 pm2 restart webhook.js
 sleep 2
-echo "pm2 restart webhook.js" >> logs/deploy.txt
+echo "\n pm2 restart webhook.js" >> logs/deploy.txt
 
-echo "\n" >> logs/deploy.txt
 pm2 list
 sleep 2
-echo "pm2 list" >> logs/deploy.txt
+echo "\n pm2 list" >> logs/deploy.txt
 
 exit
