eccfaa6807f186dccfbcfd1af68d86e568e6f2c5
nicolaslabbe <nicolas.labbe@adfab.fr>
2015-06-25 11:35:28 +0200

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,15 +9,15 @@ error_exit() {
 rm logs/deploy.txt
 pwd >> logs/deploy.txt
 
+pm2 restart webhook.js
+sleep 2
+echo "\n pm2 restart webhook.js" >> logs/deploy.txt
+
 echo "\n" >> logs/deploy.txt
 git pull origin master >> logs/deploy.txt
 sleep 2
 
 echo "deploying on ${NODE_ENV} with user ${USER} using $(node --version)" >> logs/deploy.txt
-
-pm2 restart webhook.js
-sleep 2
-echo "\n pm2 restart webhook.js" >> logs/deploy.txt
 
 pm2 list
 sleep 2
